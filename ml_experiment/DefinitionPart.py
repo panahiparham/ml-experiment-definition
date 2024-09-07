b62ab0cdb0a39cab57ad8a9c302d554ba7165fa0
Parham Panahi <panahiparham@gmail.com>
2024-09-07 11:23:50 -0600

    feat: draft builder api

---
+++ ml_experiment/DefinitionPart.py
@@ -0,0 +1,24 @@
+import os
+import sqlite3
+
+from itertools import product
+
+from typing import Dict, Iterable, Set
+from collections import defaultdict
+
+import ml_experiment._utils.sqlite as sqlu
+
+ValueType = int | float | str | bool
+
+class DefinitionPart:
+    def __init__(self, name: str, base: str | None = None):
+        self.name = name
+        self.base_path = base or os.getcwd()
+
+        self._properties: Dict[str, Set[ValueType]] = defaultdict(set)
+
+    def add_property(self, key: str, value: ValueType):
+        self._properties[key].add(value)
+
+    def add_sweepable_property(self, key: str, values: Iterable[ValueType]):
+        self._properties[key] |= set(values)
