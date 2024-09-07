7afe8b8f47245bb4b3a63238de4221a375f32863
andnp <andnpatterson@gmail.com>
2024-09-07 16:17:56 -0600

    refactor: migrate sql logic into utility classes

---
+++ ml_experiment/metadata/MetadataTable.py
@@ -0,0 +1,83 @@
+import sqlite3
+from typing import Dict, Iterable, Set
+
+# TODO: find some root-level place to store these types
+ValueType = int | float | str | bool
+
+class MetadataTable:
+    def __init__(self, part_name: str, version: int):
+        self.part_name = part_name
+        self.version = version
+
+        # cached results
+        self._cols: Set[str] | None = None
+        self._configuration_ids: Set[int] | None = None
+
+
+    def get_table_name(self):
+        return f'{self.part_name}-v{self.version}'
+
+
+    def get_columns(self, cur: sqlite3.Cursor) -> Set[str]:
+        if self._cols is not None:
+            return self._cols
+
+        res = (
+            cur.execute("PRAGMA table_info('?')", self.get_table_name())
+            .fetchall()
+        )
+        self._cols = set(x[1] for x in res)
+        return self._cols
+
+
+    def get_configuration_columns(self, cur: sqlite3.Cursor):
+        cols = self.get_columns(cur)
+        return cols - {'id'}
+
+
+    def get_configuration_ids(self, cur: sqlite3.Cursor) -> Set[int]:
+        if self._configuration_ids is not None:
+            return self._configuration_ids
+
+        res = (
+            cur.execute("SELECT DISTINCT id FROM '?'", self.get_table_name())
+            .fetchall()
+        )
+        self._configuration_ids = set(x[0] for x in res)
+        return self._configuration_ids
+
+
+    def get_configuration_id(self, cur: sqlite3.Cursor, configuration: Dict[str, ValueType]) -> int | None:
+        col_names = self.get_configuration_columns(cur)
+
+        # if this table does not have the same columns
+        # then we already know it does not have the correct id
+        # shortcutting here prevents an SQL error due to mismatching columns
+        if col_names != set(configuration.keys()):
+            return None
+
+        # otherwise, query the table for the id
+        table_name = self.get_table_name()
+        where = ' AND '.join(f'"{k}"={configuration[k]}' for k in col_names)
+
+        res = (
+            cur.execute(f"SELECT id FROM '{table_name}' WHERE {where}")
+            .fetchone()
+        )
+
+        if res is None:
+            return None
+
+        return res[0]
+
+
+    def add_configurations(self, cur: sqlite3.Cursor, configurations: Iterable[Dict[str, ValueType]]):
+        # get an ordered list of cols
+        cols = list(self.get_columns(cur))
+
+        table_name = self.get_table_name()
+        conf_str = ', '.join(['?'] * len(cols))
+        col_names = ', '.join(cols)
+        conf_values = [list(c.values()) for c in configurations]
+
+        cur.executemany(f"INSERT INTO '{table_name}' ({col_names}) VALUES ({conf_str})", conf_values)
